50f84671df72c034865117f745be200b17d6895a
Enki <ali.radif86@live.com>
2025-12-03 15:41:20 -0500

    added chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/friends_page.dart';
+import 'pages/chat_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: const HomePage(),  
+    home: const FriendsPage(),
     routes: {
       '/friends': (context) => const FriendsPage(),
+      '/friend': (context) => const ChatPage(),
+      '/home': (context) => const HomePage(),
     },
   ));
 }
---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FriendMap Chat"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
+
